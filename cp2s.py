56253cb267c176ce15107a623773e7898ce84f89
laeb <lasse.ebdrup@gmail.com>
2021-07-07 14:31:36 +0200

    Initial commit

---
+++ cp2s.py
@@ -0,0 +1,11 @@
+from serial import Serial, STOPBITS_ONE, PARITY_NONE, EIGHTBITS
+from requests import post
+
+webserver_url = "https://"
+data_dump = "cp2s_data.txt"
+with open(data_dump, 'at') as data_file:
+    with Serial('/dev/ttyUSB0', 9600, stopbits=STOPBITS_ONE, parity=PARITY_NONE, bytesize=EIGHTBITS) as ser:
+        while True:
+            line = ser.readline()
+            data_file.write(line)
+            post(webserver_url, json={'data': line})
