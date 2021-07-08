296f965cdaf51077c3ef966e04ae4b4c3f52e1be
laeb <lasse.ebdrup@gmail.com>
2021-07-08 08:29:51 +0200

    added a try clause around the whole thing so that it doesnt crap out and stop logging due to some external issue

--- cp2s.py
+++ cp2s.py
@@ -1,11 +1,14 @@
 from serial import Serial, STOPBITS_ONE, PARITY_NONE, EIGHTBITS
-from requests import post
+from requests import post, Timeout
 
 webserver_url = "https://"
 data_dump = "cp2s_data.txt"
 with open(data_dump, 'at') as data_file:
     with Serial('/dev/ttyUSB0', 9600, stopbits=STOPBITS_ONE, parity=PARITY_NONE, bytesize=EIGHTBITS) as ser:
         while True:
-            line = ser.readline()
-            data_file.write(line.decode().strip() + "\n")
-            post(webserver_url, json={'data': line.decode().strip()})
+            try:
+                line = ser.readline()
+                data_file.write(line.decode().strip() + "\n")
+                post(webserver_url, json={'data': line.decode().strip()})
+            except:  # I don't give a fuck thats its "too broad". Its meant to catch ALL exceptions!
+                pass
