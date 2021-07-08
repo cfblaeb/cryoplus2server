4e754142392e64dcc9170e815ec9ad92fce343af
laeb <lasse.ebdrup@gmail.com>
2021-07-08 13:21:15 +0200

    also send the timestamp to server

--- cp2s.py
+++ cp2s.py
@@ -13,8 +13,9 @@ with Serial('/dev/ttyUSB0', 9600, stopbits=STOPBITS_ONE, parity=PARITY_NONE, byt
         try:
             line = ser.readline()
             print(line)
-            cur.execute("INSERT INTO data VALUES (?, ?)", (datetime.now(), line.decode().strip()))
+            now_time = datetime.now()
+            cur.execute("INSERT INTO data VALUES (?, ?)", (now_time, line.decode().strip()))
             con.commit()
-            post(webserver_url, json={'data': line.decode().strip()})
-        except Exception as e:  # I don't give a fuck thats its "too broad". Its meant to catch ALL exceptions!
+            post(webserver_url, json={'date': now_time, 'data': line.decode().strip()})
+        except Exception as e:
             print(e)
